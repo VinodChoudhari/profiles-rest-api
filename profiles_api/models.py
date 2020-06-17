aa81adcadca3d03d64b7c23ba9f0b7fadd846719
Vinod Choudhari <vinod@zkteco.com>
2020-06-17 14:48:03 +0530

    Update Django version

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 
 # Create your models here.
+class UserProfile(AbstractBaseUser, Permission):
+    """Database model fo users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = modls.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
